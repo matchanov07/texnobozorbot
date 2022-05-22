4ea3039c2481f244479de0ba78a9561565e3640d
matchanov07 <otajonmatchanov070@gmail.com>
2022-05-23 02:13:15 +0500

    make it better

--- data/config.py
+++ data/config.py
@@ -1,10 +1,10 @@
-from environs import Env
+# from environs import Env
 
-# environs kutubxonasidan foydalanish
-env = Env()
-env.read_env()
+# # environs kutubxonasidan foydalanish
+# env = Env()
+# env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-PROVIDER_TOKEN = env.str('PROVIDER_TOKEN') # PayME token 
+BOT_TOKEN = "5382351855:AAFsxb5k5fixWJkLfscmmmalqCM7GT6l_Nc"  # Bot toekn
+ADMINS = [1514105989] # adminlar ro'yxati
+PROVIDER_TOKEN = '371317599:TEST:1652894381305' # PayME token 
